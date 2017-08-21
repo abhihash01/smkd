998fb34810c90a97790cb6c3a75388b1bd544640
abhilash29998 <abhilash29998@gmail.com>
2017-08-21 23:45:48 +0530

    added the flask filess

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request, session, redirect, make_response
+import datetime
+
+app = Flask(__name__)
+
+#levels start 
+@app.route('/level1')
+def level1():
+	return render_template("level11.html")
+
+#LEVEL 1 Check
+@app.route('/level1/hello')
+def truelevel2():
+	return redirect('/level2')
+
+@app.route('/level2')
+def level2():
+	return render_template("level1.html")
+
+@app.route('/level2/diamond')
+def lvl3():
+	return redirect('/level3')
+
+@app.route('/level3')
+def level3:
+        return render_template("level3.html")
+
+@app.route('/level3/<path>')
+def validateLevel3(path = ""):
+	if path == 'mist':
+		return redirect('/sweethome')
+
+@app.route('/sweethome)
+def level4():
+	return redirect("level4.html")
+
+@app.route('/sweethome/index")
+def index():
+	return redirect('/pagenotfound')
+
+@app.route('/pagenotfound')
+def pagenotfound():
+	return render_template("level5.html")
+
